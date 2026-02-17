a993f5e106bc829bd418a8e9c525f75ae54bb7b1
Luke <skibidihauktuah123@gmail.com>
2026-02-16 20:16:09 -0500

    First commit

---
+++ docs/index.md
@@ -0,0 +1,4 @@
+<title>Welcome to the Selfbot-ts documentation</title>
+
+## Welcome to the `Selfbot-ts` docs
+Thank you for using Selfbot-ts
---
+++ loader.js
@@ -0,0 +1,49 @@
+
+const renderer = {
+    link(href, title, text) {
+        const link = marked.Renderer.prototype.link.call(this, href, title, text);
+        return link.replace("<a", "<a target='_blank' rel='noreferrer' style='color: lightblue' ");
+    }
+};
+
+marked.use({
+    renderer
+});
+
+marked.use(markedKatex({
+    throwOnError: true
+}));
+
+marked.setOptions({
+  gfm: true,
+  breaks: true,
+  tables: true
+});
+
+
+Prism.plugins.toolbar.registerButton('copy-to-clipboard', function (env) {
+    const button = document.createElement('button');
+    button.innerHTML = 'Copy';
+
+    button.addEventListener('click', function () {
+        navigator.clipboard.writeText(env.code).then(function () {
+            button.textContent = 'Copied!';
+            setTimeout(function () {
+                button.textContent = 'Copy';
+            }, 2000);
+        }, function () {
+            button.textContent = 'Failed!';
+        });
+    });
+
+    return button;
+});
+
+async function load(page) {
+    const pageContent = document.getElementById("main-content")
+    const res = await fetch(`/docs/${page}.md`)
+    const text = await res.text()
+
+    pageContent.innerHTML = marked.parse(text)
+    Prism.highlightAll();
+}
